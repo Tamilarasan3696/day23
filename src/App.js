2b57f0bd592e3f7e98d709d93bae477539d6bd30
Tamilarasan <gandhitamilarasan@gmail.com>
2023-03-04 19:24:01 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Price from './component/Price';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+    <Price/>
+    </>
+    </div>
+  );
+}
+
+export default App;
